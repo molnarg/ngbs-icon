312f57fee03866cb93c5eb6d1f0dda5e7edfb15c
Gábor Molnár <894421+molnarg@users.noreply.github.com>
2024-02-17 08:47:13 +0100

    Export all interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { NgbsIconServiceClient } from './service_client'
-export { NgbsIconThermostat, NgbsIconController, NgbsIconClient } from './client'
+export * from './client'
 export { getSysId, isNgbsHost, isNgbsMacAddress } from './discovery';
 export { connect } from './url';
